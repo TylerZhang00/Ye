a64e85e1e89c92f6f36dd735986c845d3091beb8
Yiqi Zhang <yiqi@Yiqis-MacBook-Pro.local>
2022-02-18 17:08:25 -0500

    /yardages route setup

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
-
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const port = process.env.port || 3100;
 
@@ -11,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => res.send("Hello Golfers!"));
+app.use("/yardages", require("./routes/yardages"));
 
 // Start the server on port 3000
 app.listen(port, () => console.log(`Hello Golfers, welcome to port: ${port}`));
