742a78e825f39b5dbb0d899988778ecbc5324dc9
Yiqi Zhang <yiqi@Yiqis-MacBook-Pro.local>
2022-02-20 13:12:53 -0500

    updated yardage data format

--- routes/yardages.js
+++ routes/yardages.js
@@ -2,22 +2,21 @@ const router = require("express").Router();
 
 router.get("/", function (req, res) {
   console.log("Getting yardages:");
-  const yardages = {
-    sixtyDegree: [null, null],
-    assistWedge: [80, 100],
-    pitchingWedge: [125, 135],
-    nineIron: [135, 145],
-    eightIron: [145, 155],
-    sevenIron: [155, 165],
-    sixIron: [165, 175],
-    fiveIron: [175, 185],
-    fourIron: [null, null],
-    fourHybrid: [null, null],
-    threeHybrid: [null, null],
-    fiveWood: [null, null],
-    threeWood: [null, null],
-    driver: [null, null],
-  };
+  const yardages = [
+    { loftWedge: { club: "Callaway Jaws Wedge 60" }, yardages: [null, null] },
+    { sandWedge: null },
+    { assistWedge: { club: "Callaway Apex 21 AW", yardages: [80, 100] } },
+    { pitchingWedge: { club: "Callaway Apex 21 PW", yardages: [125, 135] } },
+    { nineIron: { club: "Callaway Apex 21 9I", yardages: [135, 145] } },
+    { eightIron: { club: "Callaway Apex 21 8I", yardages: [145, 155] } },
+    { sevenIron: { club: "Callaway Apex 21 7I", yardages: [155, 165] } },
+    { sixIron: { club: "Callaway Apex 21 6I", yardages: [165, 175] } },
+    { fiveIron: { club: "Callaway Apex 21 5I", yardages: [175, 185] } },
+    { fourIron: null },
+    { threeHybrid: { club: "Callaway Pro 21 Hybrid", yardages: [null, null] } },
+    { threeWood: { club: "Big Bertha 3 Wood", yardages: [null, null] } },
+    { driver: { club: "Big Bertha B21 Driver", yardages: [null, null] } },
+  ];
   console.log("Obtained yardages:", yardages);
   return res.json({
     success: true,
