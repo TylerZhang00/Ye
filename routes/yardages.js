6d171ef132bc12e9d47f31f5d6764fd9834d78de
Yiqi Zhang <yiqi@Yiqis-MacBook-Pro.local>
2022-02-20 11:51:28 -0500

    added default stock yardages

--- routes/yardages.js
+++ routes/yardages.js
@@ -1,8 +1,27 @@
 var router = require("express").Router();
 
 router.get("/", function (req, res) {
-  console.log(":rocket:");
-  res.send("Here are your stock yardages: 300 yards");
+  const yardages = {
+    sixtyDegree: [null, null],
+    assistWedge: [80, 100],
+    pitchingWedge: [125 - 135],
+    nineIron: [135 - 145],
+    eightIron: [145 - 155],
+    sevenIron: [155 - 165],
+    sixIron: [165 - 175],
+    fiveIron: [175 - 185],
+    fourIron: [null, null],
+    fourHybrid: [null, null],
+    threeHybrid: [null, null],
+    fiveWood: [null, null],
+    threeWood: [null, null],
+    driver: [null, null],
+  };
+  return res.json({
+    success: true,
+    message: "Successfully obatained yardages.",
+    yardages,
+  });
 });
 
 module.exports = router;
