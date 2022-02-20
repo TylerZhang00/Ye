d78293a652f32862e1f52504e62f9271ca546872
Yiqi Zhang <yiqi@Yiqis-MacBook-Pro.local>
2022-02-20 14:43:37 -0500

    updated yardages

--- routes/yardages.js
+++ routes/yardages.js
@@ -4,30 +4,54 @@ router.get("/", function (req, res) {
   console.log("Getting yardages:");
   const yardages = [
     {
-      club: "loftWedge",
+      club: "Loft Wedge",
       name: "Callaway Jaws Wedge 60",
       yardages: [null, null],
     },
-    { club: "sandWedge", name: null, yardages: [null, null] },
-    { club: "assistWedge", name: "Callaway Apex 21 AW", yardages: [80, 100] },
+    { club: "Sand Wedge", name: null, yardages: [null, null] },
     {
-      club: "pitchingWedge",
-      name: "Callaway Apex 21 PW",
-      yardages: [125, 135],
+      club: "Assist Wedge",
+      name: "Callaway Apex 21 AW",
+      yardages: [80, 80, 100],
     },
-    { club: "nineIron", name: "Callaway Apex 21 9I", yardages: [135, 145] },
-    { club: "eightIron", name: "Callaway Apex 21 8I", yardages: [145, 155] },
-    { club: "sevenIron", name: "Callaway Apex 21 7I", yardages: [155, 165] },
-    { club: "sixIron", name: "Callaway Apex 21 6I", yardages: [165, 175] },
-    { club: "fiveIron", name: "Callaway Apex 21 5I", yardages: [175, 185] },
-    { club: "fourIron", name: null, yardages: [null, null] },
     {
-      club: "threeHybrid",
+      club: "Pitching Wedge",
+      name: "Callaway Apex 21 PW",
+      yardages: [125, 125, 135],
+    },
+    {
+      club: "Nine Iron",
+      name: "Callaway Apex 21 9I",
+      yardages: [135, 135, 145],
+    },
+    {
+      club: "Eight Iron",
+      name: "Callaway Apex 21 8I",
+      yardages: [145, 145, 155],
+    },
+    {
+      club: "Seven Iron",
+      name: "Callaway Apex 21 7I",
+      yardages: [155, 155, 165],
+    },
+    {
+      club: "Six Iron",
+      name: "Callaway Apex 21 6I",
+      yardages: [165, 165, 175],
+    },
+    {
+      club: "Five Iron",
+      name: "Callaway Apex 21 5I",
+      yardages: [175, 175, 185],
+    },
+    { club: "Four Iron", name: null, yardages: [null, null] },
+    {
+      club: "Three Hybrid",
       name: "Callaway Pro 21 Hybrid",
       yardages: [null, null],
     },
-    { club: "threeWood", name: "Big Bertha 3 Wood", yardages: [null, null] },
-    { club: "driver", name: "Big Bertha B21 Driver", yardages: [null, null] },
+    { club: "Three Wood", name: "Big Bertha 3 Wood", yardages: [null, null] },
+    { club: "Driver", name: "Big Bertha B21 Driver", yardages: [null, null] },
   ];
   console.log("Obtained yardages:", yardages);
   return res.json({
